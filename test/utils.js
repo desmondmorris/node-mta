282c62026b4104815c7ac5bfc5a504483d827fd5
Desmond Morris <hi@desmondmorris.com>
2014-01-08 19:22:28 -0500

    Adds helper function to remove extraneous nested arrays

--- test/utils.js
+++ test/utils.js
@@ -15,3 +15,19 @@ describe('Tests merge helper', function () {
     objC.should.have.property('d', 'D');
   });
 });
+
+describe('Tests array clean function', function () {
+  it('extraneous nested arrays should be removed', function () {
+
+    var obj = {
+      element: new Array
+    };
+
+    obj.element.push({
+      foo: 'bar'
+    });
+
+    utils.arrayClean(obj).element.should.have.property('foo', 'bar');
+
+  });
+});
